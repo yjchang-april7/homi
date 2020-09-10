ab214cd93bf7fc85bc572543a1c62da22758fd92
sinsky <sinsky@mz.co.kr>
2020-09-10 11:26:17 +0900

    bump up version 0.1.2

--- src/homi/__init__.py
+++ src/homi/__init__.py
@@ -1,6 +1,5 @@
 from .app import App, Service, BaseApp, BaseService
-from .registries import get_default_registry, register
 from .server import Server
 from .test_case import HomiTestCase
 
-__version__ = "0.1.1"
+__version__ = "0.1.2"
