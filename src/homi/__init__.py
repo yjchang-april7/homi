6ce121bb801a6e3ce3bdbe39b8c8663e63a6175f
sinsky <sinsky@mz.co.kr>
2020-10-01 18:24:06 +0900

    fix flake8

--- src/homi/__init__.py
+++ src/homi/__init__.py
@@ -1,5 +1,5 @@
-from .app import App, Service, BaseApp, BaseService
+from .app import App, BaseApp, BaseService, Service
 from .server import Server
-from .test_case import HomiTestCase,HomiRealServerTestCase
+from .test_case import HomiRealServerTestCase, HomiTestCase
 
 __version__ = "0.1.6"
