54b287134eda35e43cc5961a15c9ef98e988db30
sinsky <wesky93@gmail.com>
2020-12-27 00:20:04 +0900

    init async server
    checked server is run

--- src/homi/__init__.py
+++ src/homi/__init__.py
@@ -1,4 +1,6 @@
 from .app import App, BaseApp, BaseService, Service
 from .server import Server
+from .aio.app import AsyncApp, BaseAsyncApp, BaseAsyncService, AsyncService
+from .aio.server import AsyncServer
 
 __version__ = "0.1.8"
