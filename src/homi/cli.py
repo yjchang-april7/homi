fa147c5a67f64559b7a19ee96465e7ade3f35b8c
yjchang-april7 <yjchang@april7.co.kr>
2021-04-29 14:58:04 +0900

    디버깅

--- src/homi/cli.py
+++ src/homi/cli.py
@@ -84,7 +84,7 @@ def run_command(file, port, worker, debug, alts, host=None, private_key=None, ce
 @click.option('--grpc_python_out', type=click.Path(exists=True, resolve_path=True), help='The directory of *_grpc.py', default='.')
 def protoc_command(proto_file, proto_path, python_out, grpc_python_out):
     include_proto_path = " ".join([f"--proto_path={path}" for path in proto_path])
-    
+    print(include_proto_path)
     protoc.main(
         [
             'grpc_tools.protoc',
